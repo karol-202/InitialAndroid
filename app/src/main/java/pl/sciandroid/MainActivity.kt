d0173fd354e0b648bbbae1f170ab041012c660d0
karol <karoljurski1@gmail.com>
2020-03-05 20:21:27 +0100

    Initial commit

---
+++ app/src/main/java/pl/sciandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.sciandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
